0e079ebc54320d05b4be721c7f348a3b6d2d5575
gyur1kim <qri1114@gmail.com>
2023-01-25 00:40:49 +0900

    :sparkles: Ref를 이용해 input에 focus 주기

--- 05-ref/src/Components/ValidationSample.js
+++ 05-ref/src/Components/ValidationSample.js
@@ -19,13 +19,16 @@ class ValidationSample extends Component {
     this.setState({
       clicked: true,
       validated: this.state.password === '0000'
-    })
+    });
+    // 버튼을 누르면 input에 focus가 된다! 원래는 안됐음
+    this.input.focus();
   }
 
   render() {
     return (
       <div>
         <input
+          ref={(ref) => {this.input = ref}}
           type="password"
           value={this.state.password}
           onChange={this.handleChange}
