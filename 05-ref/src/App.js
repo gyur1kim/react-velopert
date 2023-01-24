0e43527eb56d9882798dfc62876110e0d92dae1f
gyur1kim <qri1114@gmail.com>
2023-01-25 01:06:06 +0900

    :sparkles: 컴포넌트에 ref 달기

--- 05-ref/src/App.js
+++ 05-ref/src/App.js
@@ -1,13 +1,35 @@
 import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import ValidationSample from "./Components/ValidationSample";
+import ScrollBox from "./Components/ScrollBox";
 
-function App() {
-  return (
-    <div>
-      <ValidationSample />
-    </div>
-  );
+// function App() {
+//   const scrollBox = React.createRef();
+//   return (
+//     <div>
+//       <ValidationSample />
+//       <br/>
+//       <ScrollBox ref={(ref) => {const scrollBox = ref}} />
+//       <button onClick={() => {scrollBox.scrollToBottom()}}>맨 밑으로</button>
+//     </div>
+//   );
+// }
+//
+// export default App;
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ValidationSample />
+        <br/>
+        <ScrollBox ref={(ref) => {this.scrollBox = ref}} />
+        <button onClick={() => {this.scrollBox.scrollToBottom()}}>맨 밑으로</button>
+      </div>
+    );
+  }
 }
 
 export default App;
