f97f6fcbee207d5d1cf1e337c3a3c8c81e0669dc
gyur1kim <qri1114@gmail.com>
2023-02-03 23:57:00 +0900

    :sparkles: 동적인 배열을 렌더링하기
    - input text를 이용해 값을 입력받음
    - 값을 추가하면 id를 증가시키고 기존의 배열에 새로 생성된 객체를 추가
      - concat()을 이용해 기존의 배열을 변형하는 것이 아니라 새로운 배열을 생성함

--- 06-repeat-component/src/components/IterationSample.js
+++ 06-repeat-component/src/components/IterationSample.js
@@ -1,15 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const IterationSample = () => {
-  const names = ['눈사람', '얼음', '눈', '바람'];
+  // const names = ['눈사람', '얼음', '눈', '바람'];
+
   // 이 코드는 key값이 없음!
   // const nameList = names.map(name => <li>{name}</li>);
 
   // 콜백함수의 2번째 인자인 인덱스를 이용해 key값을 넣어주자!
   // 실제로는 index 값을 이용해서 key값을 넣으면 안됨!
   // 배열이 변하면 index도 변하기 때문!!
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>)
-  return <ul>{nameList}</ul>;
+  // const nameList = names.map((name, index) => <li key={index}>{name}</li>)
+
+  const [names, setNames] = useState([
+    {
+      id: 1,
+      text: '눈사람'
+    },
+    {
+      id: 2,
+      text: '얼음'
+    },
+    {
+      id: 3,
+      text: '눈'
+    },
+    {
+      id: 4,
+      text: '바람'
+    },
+  ])
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5);
+
+  const onChange = (e) => {
+    setInputText(e.target.value);
+  }
+
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText
+    });
+    setNextId(prev => prev+1);
+    setNames(nextNames);
+    setInputText('');
+  }
+
+  const nameList = names.map((name, index) => <li key={name.id}>{name.text}</li>)
+  return (
+    <>
+      <input type="text" value={inputText} onChange={onChange}/>
+      <button onClick={onClick}>추가하기</button>
+      <ul>{nameList}</ul>
+    </>
+  );
 }
 
 export default IterationSample;
