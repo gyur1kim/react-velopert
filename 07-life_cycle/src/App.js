06e306d8c530ae2d1e0a6e9b57f985817a0cd8ea
gyur1kim <qri1114@gmail.com>
2023-02-10 23:58:26 +0900

    :sparkles: 컴포넌트 생명주기 예제
    - 생성 과정
      - constructor
      - getDerivedStateFromProps(props에서 파생된 state 만들기)
      - 렌더(실제로 화면에 나오는 것이 아님)
      - componentDidMount(진짜 DOM에 마운트 됨, 화면에 보임)
    
    - update 과정
      - getDerivedStateFromProps (props에서 파생된 state 생성)
      - shouldComponentUpdate (진짜 컴포넌트 업데이트 할거니?)
      - render (화면 구성을 만드는 것)
      - getSnapshotBeforeUpdate (진짜 화면에 띄우기 전에... 이전의 props나 state 값 가지고있기 -> 다음 과정인 componentDidUpdate에서 쓰이기도 함)
      - componentDidUpdate

--- 07-life_cycle/src/App.js
+++ 07-life_cycle/src/App.js
@@ -1,13 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 import LifeCycleSample from "./LifeCycleSample";
 
-function App() {
-  return (
-    <div className="App">
-      <LifeCycleSample />
-    </div>
-  );
+function getRandomColor() {
+  return '#' + Math.floor(Math.random()* 16777215).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    );
+  }
 }
 
 export default App;
