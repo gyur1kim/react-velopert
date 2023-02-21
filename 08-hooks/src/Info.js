120fcdd0a4553e04dc44965c67513550cb4c9d3f
gyur1kim <qri1114@gmail.com>
2023-02-21 22:55:43 +0900

    :sparkles: useEffect 맛보기

--- 08-hooks/src/Info.js
+++ 08-hooks/src/Info.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Info() {
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
+  useEffect(() => {
+    console.log("렌더링이 완료됐습니다.")
+    console.log({
+      name, nickname
+    })
+  })
 
   const onChangeName = e => {
     setName(e.target.value);
