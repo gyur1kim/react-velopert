925345e4fae5ebca9cd35f15a8e0f7bdba736cb2
gyur1kim <qri1114@gmail.com>
2023-02-22 10:18:47 +0900

    :sparkles: useEffect, useReducer 적용하기
    - useReducer
      - useReducer의 첫번째 인자는 reducer 함수
        - state와 action을 인자로 받는다
        - 불변성을 지키며 업데이트 된 state를 리턴하기
        - action은 객체가 아니어도 되고, type 필드가 없어도 됨
      - 두 번째 인자는 초기 state
      - 세 번째 인자는 선택사항, initializer 함수
        - 초기 값이 계산하기 복잡할 때 lazy하기 불러올 수 있음

--- 08-hooks/src/Counter.js
+++ 08-hooks/src/Counter.js
@@ -1,13 +1,45 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+// 리듀서 함수
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT' :
+      return { value : state.value + 1 }
+    case 'DECREMENT' :
+      return { value : state.value - 1}
+    default :
+      return state
+  }
+}
+
+function initializer(initialArg) {
+  return { value : initialArg }
+}
 
 function Counter() {
-  const [value, setValue] = useState(0);
+  // 기존의 useState 작성
+  // const [value, setValue] = useState(0);
+  //
+  // return (
+  //   <div>
+  //     <p>현재 카운터 값은 <b>{value}</b>입니다</p>
+  //     <button onClick={()=>{setValue(prev => prev+1)}}>+1</button>
+  //     <button onClick={()=>{setValue(prev => prev-1)}}>-1</button>
+  //   </div>
+  // );
+
+  // useReducer 이용하기
+  // const [state, dispatch] = useReducer(reducer, { value: 0 })
+
+  // initialize state lazily with an initializer function.
+  // This is useful when creating the initial state is computationally expensive.
+  const [state, dispatch] = useReducer(reducer, 0, initializer)
 
   return (
     <div>
-      <p>현재 카운터 값은 <b>{value}</b>입니다</p>
-      <button onClick={()=>{setValue(prev => prev+1)}}>+1</button>
-      <button onClick={()=>{setValue(prev => prev-1)}}>-1</button>
+      <p>현재 카운터 값은 <b>{state.value}</b>입니다</p>
+      <button onClick={()=>{dispatch({ type: 'INCREMENT' })}}>+1</button>
+      <button onClick={()=>{dispatch({ type: 'DECREMENT' })}}>-1</button>
     </div>
   );
 }
--- 08-hooks/src/Info.js
+++ 08-hooks/src/Info.js
@@ -1,28 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
+
+function reducer (state, action) {
+  return {
+    ...state,
+    [action.name] : action.value
+  }
+}
 
 function Info() {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
-  useEffect(() => {
-    console.log("렌더링이 완료됐습니다.")
-    console.log({
-      name, nickname
-    })
-  })
+  // const [name, setName] = useState('');
+  // const [nickname, setNickname] = useState('');
+  // useEffect(() => {
+  //   console.log("렌더링이 완료됐습니다.")
+  //   console.log({
+  //     name, nickname
+  //   })
+  // })
+  //
+  // const onChangeName = e => {
+  //   setName(e.target.value);
+  // }
+  //
+  // const onChangeNickname = e => {
+  //   setNickname(e.target.value);
+  // }
 
-  const onChangeName = e => {
-    setName(e.target.value);
-  }
+  const [state, dispatch] = useReducer(reducer, {name: '', nickname: ''})
+  // 구조분해할당
+  const { name, nickname } = state;
 
-  const onChangeNickname = e => {
-    setNickname(e.target.value);
+  const onChange = e => {
+    dispatch(e.target)
   }
 
   return (
     <div>
       <div>
-        <input type="text" value={name} onChange={onChangeName}/>
-        <input type="text" value={nickname} onChange={onChangeNickname}/>
+        <input type="text" value={name} name="name" onChange={onChange}/>
+        <input type="text" value={nickname} name="nickname" onChange={onChange}/>
       </div>
       <div>
         <div><b>이름: </b>{name}</div>
