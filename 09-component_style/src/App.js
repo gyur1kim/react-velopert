7f2463db125571b16556115f79fde53fa6ffacb5
gyur1kim <qri1114@gmail.com>
2023-03-03 00:41:11 +0900

    :sparkles: CSS Module 적용하기

--- 09-component_style/src/App.js
+++ 09-component_style/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import SassComponent from "./SassComponent";
+import CSSModule from "./CSSModule";
 
 function App() {
   return (
     <div className="App">
       <SassComponent />
+      <hr/>
+      <CSSModule />
     </div>
   );
 }
