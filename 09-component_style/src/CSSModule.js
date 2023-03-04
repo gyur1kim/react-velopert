7091fe09455ead1716886aa97d86c32dfa7f2d2e
gyur1kim <qri1114@gmail.com>
2023-03-04 23:35:25 +0900

    :sparkles: classnames 모듈 사용해보기

--- 09-component_style/src/CSSModule.js
+++ 09-component_style/src/CSSModule.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from './CSSModule.module.css'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles);
 
 function CssModule() {
+  console.log(styles);
   return (
-    <div className={`${styles.wrapper} ${styles.inverted}`}>
+    // <div className={`${styles.wrapper} ${styles.inverted}`}>
+    <div className={cx('wrapper', 'inverted')}>
       안녕하세요 저는 <span className="something">CSS Module!</span>
     </div>
   );
