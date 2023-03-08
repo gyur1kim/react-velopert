a51c48de7d6ca01a0d72d2fe60fe8edb44718ce0
gyur1kim <qri1114@gmail.com>
2023-03-08 23:51:42 +0900

    :sparkles: TodoInsert
    - react코드 작성
    - css 적용

--- 10-todolist/src/App.js
+++ 10-todolist/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import TodoTemplate from "./components/TodoTemplate";
+import TodoInsert from "./components/TodoInsert";
 
 function App() {
   return (
     <TodoTemplate>
-      Todo App을 만들자!
+      <TodoInsert />
     </TodoTemplate>
   );
 }
