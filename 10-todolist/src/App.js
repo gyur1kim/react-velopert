3a1365fb0bdbdb6207d82ff999e490c3d2af093e
gyur1kim <qri1114@gmail.com>
2023-03-15 23:43:20 +0900

    :sparkles: 최적화 실습을 위해 더미데이터 생성

--- 10-todolist/src/App.js
+++ 10-todolist/src/App.js
@@ -3,24 +3,44 @@ import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링 해보기 알아보기',
-      checked: true
-    },
-    {
-      id: 3,
-      text: '일정관리 앱 만들어보기',
+function createBulkTodos() {
+  const array = [];
+  for (let i=0; i<=2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
       checked: false
-    }
-  ]);
+    })
+  }
+
+  return array;
+}
+
+function App() {
+  // const [todos, setTodos] = useState([
+  //   {
+  //     id: 1,
+  //     text: '리액트의 기초 알아보기',
+  //     checked: true
+  //   },
+  //   {
+  //     id: 2,
+  //     text: '컴포넌트 스타일링 해보기 알아보기',
+  //     checked: true
+  //   },
+  //   {
+  //     id: 3,
+  //     text: '일정관리 앱 만들어보기',
+  //     checked: false
+  //   }
+  // ]);
+
+  /*
+  * 함수를 생성할 때, 함수를 실행한 결과가 아닌 함수 자체를 넣어준다.
+  * createBulkTodos() => 리렌더링 될 때마다 함수가 호출됨
+  * createBulkTodos   => 컴포넌트가 처음 렌더링될 때만 함수가 실행됨.
+  */
+  const [todos, setTodos] = useState(createBulkTodos)
 
   // useRef를 이용해 id를 나타내는 이유
   // useState는 값이 변하면 재렌더링됨
