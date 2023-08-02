baaed9e8913361042be63e7b2163143d9016ff84
gyur1kim <qri1114@gmail.com>
2023-08-02 23:43:37 +0900

    :lipstick: 뉴스뷰어 UI 만들기
    - styled components 이용

--- 14-news-viewer/src/App.js
+++ 14-news-viewer/src/App.js
@@ -1,37 +1,44 @@
 import React, { useState } from "react";
 import axios from "axios";
+import NewsList from "./components/NewsList";
 
-function App() {
-  const [data, setData] = useState(null);
-  const NEWS_API = process.env.REACT_APP_NEWS_API;
-  console.log(NEWS_API)
+// function App() {
+//   const [data, setData] = useState(null);
+//   const NEWS_API = process.env.REACT_APP_NEWS_API;
+//   console.log(NEWS_API)
+//
+//   // then 이용
+//   // const onClick = () => {
+//   //   axios.get('https://jsonplaceholder.typicode.com/todos/1')
+//   //     .then(response => setData(response.data))
+//   //     .catch(error => console.log(error))
+//   // }
+//
+//   // async/await 이용
+//   const onClick = async () => {
+//     try {
+//       const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${NEWS_API}`);
+//       setData(response.data)
+//     }
+//     catch (e) {
+//       console.log(e)
+//     }
+//   }
+//
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={onClick}>불러오기</button>
+//       </div>
+//       { data && <textarea value={JSON.stringify(data, null, 2)} readOnly={true} cols="30" rows="10"></textarea>}
+//     </div>
+//   );
+// }
 
-  // then 이용
-  // const onClick = () => {
-  //   axios.get('https://jsonplaceholder.typicode.com/todos/1')
-  //     .then(response => setData(response.data))
-  //     .catch(error => console.log(error))
-  // }
-
-  // async/await 이용
-  const onClick = async () => {
-    try {
-      const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${NEWS_API}`);
-      setData(response.data)
-    }
-    catch (e) {
-      console.log(e)
-    }
-  }
-
+const App = () => {
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      { data && <textarea value={JSON.stringify(data, null, 2)} readOnly={true} cols="30" rows="10"></textarea>}
-    </div>
-  );
+    <NewsList />
+  )
 }
 
 export default App;
---
+++ 14-news-viewer/src/components/NewsList.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from "styled-components";
+import NewsItem from "./NewsItem";
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 2rem auto 0;
+  
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    margin-left: 1rem;
+    margin-right: 1rem;
+  }
+`
+
+const sampleArticle = {
+  title: "제목",
+  description: "내용",
+  url: "https://google.com",
+  urlToImage: "https://via.placeholder.com/160"
+}
+
+function NewsList(props) {
+  return (
+    <NewsListBlock>
+      <NewsItem article={sampleArticle}/>
+      <NewsItem article={sampleArticle}/>
+      <NewsItem article={sampleArticle}/>
+      <NewsItem article={sampleArticle}/>
+      <NewsItem article={sampleArticle}/>
+      <NewsItem article={sampleArticle}/>
+      <NewsItem article={sampleArticle}/>
+      <NewsItem article={sampleArticle}/>
+      <NewsItem article={sampleArticle}/>
+    </NewsListBlock>
+  );
+}
+
+export default NewsList;
