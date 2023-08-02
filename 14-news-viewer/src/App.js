207911f1b5763b83118ac729037697f62aa31982
gyur1kim <qri1114@gmail.com>
2023-08-02 23:10:30 +0900

    :sparkles: promise 객체 맛보기
    - then을 이용해 서버로부터 값 받아오기
    - async/await를 이용해 서버로부터 값 받아오기

--- 14-news-viewer/src/App.js
+++ 14-news-viewer/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  // then 이용
+  // const onClick = () => {
+  //   axios.get('https://jsonplaceholder.typicode.com/todos/1')
+  //     .then(response => setData(response.data))
+  //     .catch(error => console.log(error))
+  // }
+
+  // async/await 이용
+  const onClick = async () => {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+      setData(response.data)
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      { data && <textarea value={JSON.stringify(data, null, 2)} readOnly={true} cols="30" rows="10"></textarea>}
     </div>
   );
 }
