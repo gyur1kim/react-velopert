e55feaaaa9f76ccc851de9f5d50641a258fd56d6
gyur1kim <qri1114@gmail.com>
2023-09-03 23:40:04 +0900

    :sparkles: useContext를 이용해 Consumer를 대신하기

--- 15-context_api/src/components/ColorBox.jsx
+++ 15-context_api/src/components/ColorBox.jsx
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {ColorConsumer} from "../contexts/color";
+import ColorContext from "../contexts/color";
 
 function ColorBox() {
+  const { state } = useContext(ColorContext);
   return (
-    <ColorConsumer>
-      { value => (
+    // <ColorConsumer>
+    //   { value => (
           <>
             <div
               style={{
                 width: "64px",
                 height: "64px",
-                background: value.state.color
+                // background: value.state.color
+                background: state.color
               }}
             />
             <div
               style={{
                 width: "32px",
                 height: "32px",
-                background: value.state.subColor
+                // background: value.state.subColor
+                background: state.subColor
               }}
             />
           </>
-      )}
-    </ColorConsumer>
+    //   )}
+    // </ColorConsumer>
   );
 }
 
